3fc2e283eb2b2589cbb14ec672932e5084a71710
Balashangar Kailasapathy <87571423+BKailasapathy@users.noreply.github.com>
2021-09-10 17:08:51 +0530

    Update Myheaderfile1.h

--- interface/Myheaderfile1.h
+++ interface/Myheaderfile1.h
@@ -1,12 +1,14 @@
 #include "fastjet/PseudoJet.hh"
 
+int _pdg_id;         // the associated pdg id
+int _vertex_number;  // the associated vertex number
+
 class Myheaderfile1 : public fastjet::PseudoJet::UserInfoBase{
  public:
    // default ctor
    //  - pdg_id        the PDG id of the particle
    //  - vertex_number theid of the vertex it originates from
-   Myheaderfile1(const int & pdg_id_in, const int & vertex_number_in) :
-     _pdg_id(pdg_id_in), _vertex_number(vertex_number_in){}
+   Myheaderfile1(const int & pdg_id_in, const int & vertex_number_in) : _pdg_id(pdg_id_in), _vertex_number(vertex_number_in){}
  
    /// access to the PDG id
    int pdg_id() const { return _pdg_id;}
@@ -15,6 +17,5 @@ class Myheaderfile1 : public fastjet::PseudoJet::UserInfoBase{
    int vertex_number() const { return _vertex_number;}
    
 // protected:
-//   int _pdg_id;         // the associated pdg id
-//   int _vertex_number;  // the associated vertex number
+
  };
